f77f64877bcebef6f78be1ad27c6ab2d1e8d13d8
Max <patronsgg@yandex.ru>
2021-03-13 16:57:35 +0300

    update to the fourth task

--- sever.py
+++ sever.py
@@ -3,14 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/list_prof/<list>')
-def list_prof(list):
-    return render_template('index.html', list=list, list_prof=['Инженер-исследователь', 'Пилот', 'Строитель',
-                                                               'Экзобиолог', 'Врач', 'Инженер по терраформированию',
-                                                               'Климатолог', 'Специалист по радиационной защите',
-                                                               'Астрогеолог', 'Гляциолог', 'Инженер жизнеобеспечения',
-                                                               'Метеоролог', 'Оператор марсохода', 'Киберинженер',
-                                                               'Штурман', 'Пилот дронов'])
+@app.route('/answer/')
+@app.route('/auto_answer/')
+def answer():
+    param = {}
+    param['title'] = 'Анкета'
+    param['surname'] = ('Фамилия:', 'Watny')
+    param['name'] = ('Имя:', 'Mark')
+    param['education'] = ('Образование:', 'выше среднего')
+    param['profession'] = ('Профессия:', 'штурман марсохода')
+    param['sex'] = ('Пол:', 'male')
+    param['motivation'] = ('Мотивация:', 'Всегда мечтал застрять на Марсе!')
+    param['ready'] = ('Готовы остаться на Марсе?', 'True')
+    return render_template('auto_answer.html', param=param)
 
 
 if __name__ == '__main__':
