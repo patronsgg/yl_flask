f50c719df23ab611324fcf9f43a44374ca8da520
Max <patronsgg@yandex.ru>
2021-03-13 23:03:11 +0300

    Update to the sixth task

--- sever.py
+++ sever.py
@@ -1,26 +1,12 @@
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-class AccessForm(FlaskForm):
-    id_astronaut = StringField('ID астронавта', validators=[DataRequired(message='Необходимо выбрать значение')])
-    password_astronaut = PasswordField('Пароль', validators=[DataRequired(message='Необходимо выбрать значение')])
-    id_captain = StringField('ID капитана', validators=[DataRequired(message='Необходимо выбрать значение')])
-    password_captain = PasswordField('Пароль', validators=[DataRequired(message='Необходимо выбрать значение')])
-    submit = SubmitField('Доступ')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = AccessForm()
-    if form.validate_on_submit():
-        return "Доступ разрешен"
-    return render_template('login.html', form=form)
+@app.route('/distribution')
+def distribution():
+    return render_template('index.html', persons=['Ридли Скотт', 'Энди Уир', 'Марк Уотни',
+                                                  'Венката Капур', 'Тедди Сандерс', 'Шон Бин'])
 
 
 if __name__ == '__main__':
