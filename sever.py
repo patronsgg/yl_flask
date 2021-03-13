d2bf5545022d0ffdd6b13f9a7601ac62db94adf6
Max <patronsgg@yandex.ru>
2021-03-13 15:53:50 +0300

    update to second task

--- sever.py
+++ sever.py
@@ -3,10 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def index(title):
-    return render_template('base.html', title=title)
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('index.html', prof=prof)
 
 
 if __name__ == '__main__':
