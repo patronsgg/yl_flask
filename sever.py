24421d08b276f5fcde60cbcc539d3b944c75a3b7
Max <patronsgg@yandex.ru>
2021-03-13 17:41:15 +0300

    Update to the fifth task

--- sever.py
+++ sever.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/answer/')
-@app.route('/auto_answer/')
-def answer():
-    param = {}
-    param['title'] = 'Анкета'
-    param['surname'] = ('Фамилия:', 'Watny')
-    param['name'] = ('Имя:', 'Mark')
-    param['education'] = ('Образование:', 'выше среднего')
-    param['profession'] = ('Профессия:', 'штурман марсохода')
-    param['sex'] = ('Пол:', 'male')
-    param['motivation'] = ('Мотивация:', 'Всегда мечтал застрять на Марсе!')
-    param['ready'] = ('Готовы остаться на Марсе?', 'True')
-    return render_template('auto_answer.html', param=param)
+class AccessForm(FlaskForm):
+    id_astronaut = StringField('ID астронавта', validators=[DataRequired(message='Необходимо выбрать значение')])
+    password_astronaut = PasswordField('Пароль', validators=[DataRequired(message='Необходимо выбрать значение')])
+    id_captain = StringField('ID капитана', validators=[DataRequired(message='Необходимо выбрать значение')])
+    password_captain = PasswordField('Пароль', validators=[DataRequired(message='Необходимо выбрать значение')])
+    submit = SubmitField('Доступ')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = AccessForm()
+    if form.validate_on_submit():
+        return "Доступ разрешен"
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
